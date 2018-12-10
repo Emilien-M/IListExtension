d721861b5cccac61ca34674075ee051b707d69aa
Emilien Mathieu <emilien.mathieu@equadex.net>
2018-12-10 23:49:40 +0100

    Add Exists method Add Find method Add FindAll method Add FindLast method Add FindLastIndex method

--- src/IListExtension/IListExtension.cs
+++ src/IListExtension/IListExtension.cs
@@ -1,39 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IListExtension
 {
     public static class IListExtension
     {
-        public static int RemoveAll<T>(this IList<T> source, Predicate<T> predicate)
-        {
-            if (source == null || predicate == null)
-            {
-                return -1;
-            }
-
-            if (source is List<T> concreteList)
-            {
-                return concreteList.RemoveAll(predicate);
-            }
-
-            int result = 0;
-
-            for (int i = source.Count - 1; i >= 0; i--)
-            {
-                if (!predicate(source[i]))
-                {
-                    continue;
-                }
-
-                ++result;
-                source.RemoveAt(i);
-            }
-
-            return result;
-        }
-
         public static void AddRange<T>(this IList<T> source, IEnumerable<T> newList)
         {
             if (source == null || newList == null)
@@ -51,6 +24,175 @@ namespace IListExtension
             {
                 source.Add(element);
             }
+        }
+        
+        public static bool Exists<T>(this IList<T> source, Predicate<T> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            if (source is List<T> concreteList)
+            {
+                return concreteList.Exists(predicate);
+            }
+           
+            foreach (T element in source)
+            {
+                if (predicate(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+        public static T Find<T>(this IList<T> source, Predicate<T> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+            
+            if (source is List<T> concreteList)
+            {
+                return concreteList.Find(predicate);
+            }
+            
+            foreach (T element in source)
+            {
+                if (predicate(element))
+                {
+                    return element;
+                }
+            }
+
+            return default;
+        }
+
+        public static IList<T> FindAll<T>(this IList<T> source, Predicate<T> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+            
+            if (source is List<T> concreteList)
+            {
+                return concreteList.FindAll(predicate);
+            }
+            
+            IList<T> results = new List<T>();
+
+            foreach (T element in source)
+            {
+                if (predicate(element))
+                {
+                    results.Add(element);
+                }
+            }
+
+            return results;
+        }
+        
+        public static int FindIndex<T>(this IList<T> source, Predicate<T> predicate)
+        {
+            if (source == null || predicate == null)
+            {
+                return -1;
+            }
+
+            if (source is List<T> concreteList)
+            {
+                return concreteList.RemoveAll(predicate);
+            }
+
+            for (int i = 0; i < source.Count; i++)
+            {
+                if (predicate(source[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+        
+        public static T FindLast<T>(this IList<T> source, Predicate<T> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+            
+            if (source is List<T> concreteList)
+            {
+                return concreteList.FindLast(predicate);
+            }
+
+            T result = default;
+            
+            foreach (T element in source)
+            {
+                if (predicate(element))
+                {
+                    result = element;
+                }
+            }
+
+            return result;
+        }
+        
+        public static int FindLastIndex<T>(this IList<T> source, Predicate<T> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+            
+            if (source is List<T> concreteList)
+            {
+                return concreteList.FindLastIndex(predicate);
+            }
+
+            int result = -1;
+            
+            foreach (T element in source)
+            {
+                if (predicate(element))
+                {
+                    result = source.IndexOf(element);
+                }
+            }
+
+            return result;
         }
 
         public static void ForEach<T>(this IList<T> source, Action<T> action)
@@ -85,7 +227,7 @@ namespace IListExtension
             }
         }
 
-        public static int FindIndex<T>(this IList<T> source, Predicate<T> predicate)
+        public static int RemoveAll<T>(this IList<T> source, Predicate<T> predicate)
         {
             if (source == null || predicate == null)
             {
@@ -97,15 +239,30 @@ namespace IListExtension
                 return concreteList.RemoveAll(predicate);
             }
 
-            for (int i = 0; i < source.Count; i++)
+            int result = 0;
+
+            for (int i = source.Count - 1; i >= 0; i--)
             {
-                if (predicate(source[i]))
+                if (!predicate(source[i]))
                 {
-                    return i;
+                    continue;
                 }
+
+                ++result;
+                source.RemoveAt(i);
             }
 
-            return -1;
+            return result;
         }
+
+        //AsReadOnly
+        //BinarySearch
+        //ConvertAll
+        //GetRange
+        //InsertRange
+        //RemoveRange
+        //Sort
+        //TrimExcess
+        //TrueForAll
     }
 }
